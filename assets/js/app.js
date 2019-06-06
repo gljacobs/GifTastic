a40e4b1c714272bbf7cb7e5a459dfb22a4f2bcc8
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-06-06 15:00:20 -0700

    get and render gifs on button click

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ var topics = ["wolf", "lizard", "tiger", "bear"];
 
 var renderButton = (arr) => {
     $("#buttons").empty();
-    for(var i = 0; i < arr.length; i++){
+    for (var i = 0; i < arr.length; i++) {
         var butt = $("<button>" + arr[i] + "</button>");
         butt.attr("id", i);
         butt.val(arr[i]);
@@ -15,6 +15,34 @@ $("#submit").on("click", (event) => {
     topics.push($("#search").val());
     $("#search").val("");
     renderButton(topics);
-})
+});
 
 renderButton(topics);
+
+$("button").on("click", function () {
+    var animal = $(this).val();
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        animal + "&api_key=JMG89CzdvjAg1S68HR88ZX9xfKv2PHsK";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        console.log(response);
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+
+            var animalDiv = $("<div>");
+            var p = $("<p>");
+            p.text(results[i].rating);
+            var img = $("<img>");
+            img.attr("src", results[i].images.fixed_height.url);
+            animalDiv.append(p);
+            animalDiv.append(img);
+            $("#gifs").prepend(animalDiv);
+
+        }
+    });
+});
