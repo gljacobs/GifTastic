091576d6c5ab01c93c3998b639a5b6ccd9fd94bf
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-06-06 09:14:19 -0700

    link css and js/jquery to html, add button rendering functionality

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+var topics = ["wolf", "lizard", "tiger", "bear"];
+
+var renderButton = (arr) => {
+    $("#buttons").empty();
+    for(var i = 0; i < arr.length; i++){
+        var butt = $("<button>" + arr[i] + "</button>");
+        butt.attr("id", i);
+        butt.val(arr[i]);
+        $("#buttons").append(butt);
+    }
+}
+
+$("#submit").on("click", (event) => {
+    event.preventDefault();
+    topics.push($("#search").val());
+    renderButton(topics);
+})
+
+renderButton(topics);
